d97b23bf426b71af0bd50b585765162694c85bbb
T-O-R-U-S <mahmoud_moursy@icloud.com>
2024-08-05 09:02:50 +0400

    Added documentation for dummy widget.

--- src/terminal/dummy.rs
+++ src/terminal/dummy.rs
@@ -2,6 +2,14 @@ use crate::prelude::Metadata;
 use crate::style::Style;
 use crate::terminal::{Cell, TerminalConst, TerminalMut};
 
+#[allow(unused_imports)] // used in docs
+use crate::terminal::Terminal;
+
+/// A [`Terminal`] implementation that doesn't do all that much...
+///
+/// Returns an empty [`Iterator`] when calling [`TerminalConst::cells`] or [`TerminalMut::cells_mut`].
+///
+/// Dimensions from [`Metadata`] are (0,0), and default style is always the empty style.
 #[derive(Ord, PartialOrd, Eq, PartialEq, Debug, Default, Hash, Copy, Clone)]
 pub struct Dummy;
 
