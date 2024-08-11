5074577e8884196e88f7eab8ea63bff530d86470
Mahmoud Moursy <mahmoud_moursy@icloud.com>
2024-08-11 08:28:14 +0400

    Removed the old centered_text.rs and centered_prompt.rs widgets.

--- src/widgets/builtins/mod.rs
+++ src/widgets/builtins/mod.rs
@@ -1,5 +1,3 @@
-pub use centered_prompt::CenteredPrompt;
-pub use centered_text::CenteredText;
 pub use ruler::Ruler;
 pub use sweeper::Sweeper;
 pub use text::Text;
@@ -15,10 +13,6 @@ use crate::widgets::BoundingBox;
 
 /// The code for the [`Sweeper`] widget.
 pub mod sweeper;
-/// The code for the [`CenteredText`] widget.
-pub mod centered_text;
-/// The code for the [`CenteredPrompt`] widget.
-pub mod centered_prompt;
 /// The code for the [`Ruler`] widget.
 pub mod ruler;
 /// The code for the [`Text`] widget.
